c5c8c364b89a167febc3fe96c8366c6e00e5b341
Sho Arata <arata.sho@gmail.com>
2025-03-12 09:43:44 -0400

    created dummy producer

--- system_logs_generator.py
+++ system_logs_generator.py
@@ -1,3 +1,17 @@
-from confluent_kafka import Consumer
+from confluent_kafka import Producer
+import time
 
-a = Consumer()
+config = {
+    "bootstrap.servers": "localhost:9092"
+}
+producer = Producer(config)
+def del_callback(err, msg):
+    if err:
+        print(f"{err=}")
+    else:
+        print(f"{msg.topic()=}, {msg.key()=}, {msg.value()=}")
+for i in range (10):
+    time.sleep(2)
+    producer.produce(topic="system_logs", value=f"this is a test log {i}", key="ShoArata", callback=del_callback)
+producer.flush()
+
